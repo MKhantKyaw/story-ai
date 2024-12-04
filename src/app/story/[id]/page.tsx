66fa8c5fb74c32d41a99af34a0e3931f03b82ce0
Min Khant Kyaw <mkhantkyaw61@gmail.com>
2024-12-04 16:08:47 +0000

    Refactor StoryPage component to use Promise for params type definition

--- src/app/story/[id]/page.tsx
+++ src/app/story/[id]/page.tsx
@@ -1,13 +1,14 @@
 import StoryDisplay from "@/components/display-story";
 import { getStory } from "@/lib/actions";
 
-type Props = {
-  params: {
+type PageProps = {
+  //Remark for myself: I have to use Promise<> to get the params from Next 15
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 
-export default async function StoryPage({ params }: Props) {
+export default async function StoryPage({ params }: PageProps) {
   const { id } = await params;
   const story = await getStory(id);
   return (
